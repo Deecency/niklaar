f0f0f278af4b2a105efb39ac69355f105c5f88ff
Decency Anukam <124494565+Deecency@users.noreply.github.com>
2025-03-05 14:12:39 +0100

    upgrade ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,9 +8,9 @@ plugins {
 android {
     namespace = "com.example.smart_rob"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
-    compileOptions {
+    compileOptions {git commit -m "Initial commit: Add project files"
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
